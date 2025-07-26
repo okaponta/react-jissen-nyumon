6c7d276667a239db87041e4b1501a861697bb80f
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-07-27 00:41:54 +0900

    3-3-6

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import StateBasic from './chap3/StateBasic';
+import StateParent from './chap3/StateParent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StateBasic init={0} />
+    <StateParent />
   </React.StrictMode>
 );
 
