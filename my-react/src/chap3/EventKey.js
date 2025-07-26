746b842e99cbcb08fb2f6bea6c35a98a3adaed18
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-07-27 01:06:14 +0900

    3-4-2

---
+++ my-react/src/chap3/EventKey.js
@@ -0,0 +1,16 @@
+export default function EventKey() {
+    const handleKey = e => {
+        if (e.ctrlKey && e.key === 'q') {
+            alert('Ctrl + Q が押されました');
+        }
+    }
+
+    return (
+        <form>
+            <label>
+                名前：
+                <input type="text" size="20" onKeyDown={handleKey} />
+            </label>
+        </form>
+    )
+}
