d038b8834cfa6241384a198d5116624e74aeacfd
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-07-29 12:02:30 +0900

    4-1-3

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import StateForm from './chap4/StateForm';
+import StateFormUC from './chap4/StateFormUC';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StateForm />
+    <StateFormUC />
   </React.StrictMode>
 );
 
