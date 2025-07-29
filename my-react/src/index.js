6949af544a55c44e5b3eb60b2a07635a3d9457cf
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-07-29 16:04:16 +0900

    4-2-2

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import StateNest from './chap4/StateNest';
+import StateTodo from './chap4/StateTodo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StateNest />
+    <StateTodo />
   </React.StrictMode>
 );
 
