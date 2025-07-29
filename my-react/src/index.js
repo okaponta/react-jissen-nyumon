9008e0ccf5201208954b65f422930f4ad86c1c26
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-07-29 17:22:28 +0900

    4-3-3

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import StateTodo from './chap4/StateTodo';
+import FormBasic from './chap4/FormBasic';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StateTodo />
+    <FormBasic />
   </React.StrictMode>
 );
 
