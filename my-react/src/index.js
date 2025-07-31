48f59109c47176822ea93b96541a919dbaab7cf4
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-07-31 11:49:11 +0900

    6-1-2

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import ErrorEventRoot from './chap5/ErrorEventRoot';
+import MaterialBasic from './chap6/MaterialBasic';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ErrorEventRoot />
+    <MaterialBasic />
   </React.StrictMode>
 );
 
