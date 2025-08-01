140ee6c4db4315e053bc19e055fea617c8acd173
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-01 13:13:23 +0900

    7-2-3

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import HookRefForward from "./chap7/HookRefForward";
+import HookCallbackRef from "./chap7/HookCallbackRef";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HookRefForward />
+    <HookCallbackRef />
   </React.StrictMode>
 );
 
