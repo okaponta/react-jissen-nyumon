0e3d7cfd37f0da25b28a0f0b74d08940346cf7d8
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-02 03:28:06 +0900

    7-6-4

--- my-react/src/chap7/HookMemo.js
+++ my-react/src/chap7/HookMemo.js
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { MyButton, MyCounter } from "./HookMemoChild";
 
 const sleep = (delay) => {
@@ -10,8 +10,8 @@ export default function HookMemo() {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
 
-  const increment = () => setCount1((c) => c + 1);
-  const decrement = () => setCount2((c) => c - 1);
+  const increment = useCallback(() => setCount1((c) => c + 1), []);
+  const decrement = useCallback(() => setCount2((c) => c - 1), []);
 
   const heavyProcess = useMemo(() => {
     sleep(1000);
