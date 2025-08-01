e2dceab76441c802813fea7eedd43113d1ab0f63
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-01 10:36:20 +0900

    format

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import MaterialDrawer from './chap6/MaterialDrawer';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import MaterialDrawer from "./chap6/MaterialDrawer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <MaterialDrawer />
