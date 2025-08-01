ea0edcdd4a36d8e5eb9623a8f2affae616974141
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-01 11:01:55 +0900

    6-2

--- my-react/src/chap6/MyButton.js
+++ my-react/src/chap6/MyButton.js
@@ -1,10 +1,15 @@
+import PropTypes from "prop-types";
 import "../stories/button.css";
 
+/**
+ * 属性の設定に応じて種々のボタンを生成
+ */
 export default function MyButton({
   primary = false,
   backgroundColor = null,
   size = "medium",
   label = "Button",
+  handleClick,
   ...props
 }) {
   const mode = primary
@@ -17,9 +22,35 @@ export default function MyButton({
         " "
       )}
       style={backgroundColor && { backgroundColor }}
+      onClick={handleClick}
       {...props}
     >
       {label}
     </button>
   );
 }
+
+// Propsの型情報を宣言
+/**
+ * Primaryカラーを有効にするか
+ */
+MyButton.propTypes = {
+  primary: PropTypes.bool,
+  /**
+   * 背景色
+   */
+  backgroundColor: PropTypes.string,
+  /**
+   * ボタンの大きさ
+   */
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  /**
+   * ボタンのキャプション
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * clickハンドラー
+   */
+  onClick: PropTypes.func,
+  // handleClick: PropTypes.func,
+};
