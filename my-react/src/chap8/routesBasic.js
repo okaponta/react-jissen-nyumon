3286f6f61dd920ff7f0e17527efd72d0ad1f4550
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-04 17:16:29 +0900

    8-1-3

--- my-react/src/chap8/routesBasic.js
+++ my-react/src/chap8/routesBasic.js
@@ -1,12 +1,20 @@
-import { createBrowserRouter } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import TopPage from "./TopPage";
 import ArticlePage from "./ArticlePage";
 import AboutPage from "./AboutPage";
 
-const routesBasic = createBrowserRouter([
-  { path: "/", element: <TopPage /> },
-  { path: "article", element: <ArticlePage /> },
-  { path: "/about", element: <AboutPage /> },
-]);
+const routesBasic = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<TopPage />} />
+      <Route path="/article" element={<ArticlePage />} />
+      <Route path="/about" element={<AboutPage />} />
+    </>
+  )
+);
 
 export default routesBasic;
