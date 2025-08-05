f783b739c66a0fa91d271435a1871871a01de5b6
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-05 16:19:02 +0900

    format

--- my-react/src/chap8/MyHeader.js
+++ my-react/src/chap8/MyHeader.js
@@ -1,15 +1,15 @@
-import { Helmet } from 'react-helmet-async';
-import { useMatches } from 'react-router-dom';
+import { Helmet } from "react-helmet-async";
+import { useMatches } from "react-router-dom";
 
 export default function MyHeader(props) {
   let {
-    title = 'React入門',
-    keywords = 'React, JavaScript, フレームワーク',
-    description = 'React入門のサンプルです。'
+    title = "React入門",
+    keywords = "React, JavaScript, フレームワーク",
+    description = "React入門のサンプルです。",
   } = useMatches().at(-1).handle ?? {};
-  title = title.replace('%s', props.title);
-  keywords = keywords.replace('%s', props.keywords);
-  description = description.replace('%s', props.description);
+  title = title.replace("%s", props.title);
+  keywords = keywords.replace("%s", props.keywords);
+  description = description.replace("%s", props.description);
   return (
     <Helmet>
       <title>{title}</title>
